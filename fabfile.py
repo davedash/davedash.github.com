591b71eb2ebba0aaecf07a6898484e571b69ebfe
Dave Dash <dd+github@davedash.com>
2010-08-18 09:12:21 -0700

    FabFile

---
+++ fabfile.py
@@ -0,0 +1,10 @@
+from fabric.api import local
+
+
+def tagged(tag):
+    """Show posts tagged with ``tag``"""
+
+    # Open all files in _posts
+
+    # Parse the yml section
+    # Remember the file names that have tags matching tag
